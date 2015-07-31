67de8adf53cf6484f28de82ab2b49665f18eb1de
Ben Sechrist <bensechrist@gmail.com>
2015-07-31 09:56:11 -0400

    Cut usage of bitmaps in memory by half, increasing performance

--- app/src/main/java/com/bt4vt/repository/domain/Departure.java
+++ app/src/main/java/com/bt4vt/repository/domain/Departure.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,57 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.bt4vt.repository.domain;
 
-import org.simpleframework.xml.Element;
-import org.simpleframework.xml.Root;
-
 /**
- * Transit stop departure
- *
  * @author Ben Sechrist
  */
-@Root(name = "NextDepartures")
 public class Departure {
-
-  @Element(name = "RouteName")
-  private String routeName;
-
-  @Element(name = "AdjustedDepartureTime_TripNotes")
-  private String notes;
-
-  public String getRouteName() {
-    return routeName;
-  }
-
-  public String getNotes() {
-    return notes;
-  }
-
-  @Override
-  public int hashCode() {
-    String[] array = new String[]{routeName, notes};
-    int hashCode = 1;
-    for (Object element : array) {
-      int elementHashCode;
-
-      if (element == null) {
-        elementHashCode = 0;
-      } else {
-        elementHashCode = (element).hashCode();
-      }
-      hashCode = 31 * hashCode + elementHashCode;
-    }
-    return hashCode;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == this) return true;
-    if (!(o instanceof Departure)) return false;
-    Departure that = (Departure) o;
-    return this.routeName.equals(that.routeName) &&
-        this.notes.equals(that.notes);
-  }
 }
