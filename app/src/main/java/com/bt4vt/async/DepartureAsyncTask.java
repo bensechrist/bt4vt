b2761ff26d6519b8942b14a9320016c0b6695c35
Ben Sechrist <bensechrist@gmail.com>
2015-11-07 21:28:45 -0600

    Added lookahead limit to displayed departures
    If the lookahead limit is 60 minutes then only departures in the next 60 minutes will be displayed.

--- app/src/main/java/com/bt4vt/async/DepartureAsyncTask.java
+++ app/src/main/java/com/bt4vt/async/DepartureAsyncTask.java
@@ -16,10 +16,13 @@
 
 package com.bt4vt.async;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
+import com.bt4vt.R;
 import com.bt4vt.repository.TransitRepository;
 import com.bt4vt.repository.domain.Departure;
 import com.bt4vt.repository.domain.NextDeparture;
@@ -29,41 +32,59 @@ import com.bt4vt.repository.exception.TransitRepositoryException;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Fetches departures on a separate thread.
  *
  * @author Ben Sechrist
  */
-public class DepartureAsyncTask extends AsyncTask<Void, Integer, List<Departure>> {
+public class DepartureAsyncTask extends AsyncTask<Void, Void, List<Departure>> {
+
+  private static final String TAG = "DepartureAsyncTask";
 
   private final TransitRepository transitRepository;
   private final Stop stop;
   private final Route route;
   private final AsyncCallback<List<Departure>> callback;
+  private final Context context;
 
   public DepartureAsyncTask(TransitRepository transitRepository, Stop stop, Route route,
-                            AsyncCallback<List<Departure>> callback) {
+                            AsyncCallback<List<Departure>> callback, Context context) {
     this.transitRepository = transitRepository;
     this.stop = stop;
     this.route = route;
     this.callback = callback;
+    this.context = context;
   }
 
   @Override
   protected List<Departure> doInBackground(Void... params) {
     try {
       List<Departure> departures = new ArrayList<>();
+      int MAX_DEPARTURE_LOOKAHEAD_MINS = context.getResources().getInteger(R.integer.max_departure_lookahead_mins);
       for (NextDeparture nd : transitRepository.getNextDepartures(stop, route)) {
-        departures.addAll(nd.getDepartures());
+        for (Departure d : nd.getDepartures()) {
+          if (isBefore(MAX_DEPARTURE_LOOKAHEAD_MINS, d)) {
+            departures.add(d);
+          }
+        }
       }
       Collections.sort(departures);
+      Log.d(TAG, departures.toString());
       return departures;
     } catch (final TransitRepositoryException e) {
       new Handler(Looper.getMainLooper()).post(new CallbackExceptionRunnable(callback, e));
       return null;
     }
+  }
+
+  private boolean isBefore(int MAX_DEPARTURE_LOOKAHEAD_MINS, Departure d) {
+    return d.getDepartureTime().before(
+        new Date(new Date().getTime() +
+            TimeUnit.MILLISECONDS.convert(MAX_DEPARTURE_LOOKAHEAD_MINS, TimeUnit.MINUTES)));
   }
 
   @Override
