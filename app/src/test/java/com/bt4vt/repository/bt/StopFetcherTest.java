b2da2d6c21897a5da664215351b1acbbbe66300b
Ben Sechrist <bensechrist@gmail.com>
2016-09-14 23:08:21 -0500

    Optimized imports

--- app/src/test/java/com/bt4vt/repository/bt/StopFetcherTest.java
+++ app/src/test/java/com/bt4vt/repository/bt/StopFetcherTest.java
@@ -22,9 +22,9 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Tests the {@link StopFetcher}.
