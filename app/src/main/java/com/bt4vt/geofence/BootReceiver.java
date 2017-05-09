c2540c7e74d2f8708bdff88eece9a3abd9eaa9d4
Ben Sechrist <bensechrist@gmail.com>
2017-05-08 21:33:53 -0500

    Set geofences on boot

--- app/src/main/java/com/bt4vt/geofence/BootReceiver.java
+++ app/src/main/java/com/bt4vt/geofence/BootReceiver.java
@@ -16,43 +16,61 @@
 
 package com.bt4vt.geofence;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.bt4vt.R;
+import com.bt4vt.external.bt4u.Response;
+import com.bt4vt.external.bt4u.Stop;
+import com.bt4vt.external.bt4u.StopService;
+import com.bt4vt.model.FavoriteStop;
+import com.bt4vt.service.FavoriteStopService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.inject.Inject;
+
+import java.util.List;
 
 import roboguice.receiver.RoboBroadcastReceiver;
 
-public class BootReceiver extends RoboBroadcastReceiver {
+public class BootReceiver extends RoboBroadcastReceiver implements Response.Listener<Stop>,
+    Response.ExceptionListener {
 
   private static final String TAG = "BootReceiver";
 
   @Inject
+  private FavoriteStopService favoriteStopService;
+
+  @Inject
+  private StopService stopService;
+
+  @Inject
   private BusStopGeofenceService busStopGeofenceService;
 
-//  @Override
-//  public void handleReceive(Context context, Intent intent) {
-//    Log.d(TAG, "Received boot");
-//    if (GoogleApiAvailability.getInstance()
-//        .isGooglePlayServicesAvailable(context) != ConnectionResult.SUCCESS) {
-//      Log.e(TAG, context.getString(R.string.no_play_services));
-//    } else {
-//      List<StopFavorite> stopFavorites = new Select().from(StopFavorite.class)
-//          .where("isFavorited = ?", true).execute();
-//      Log.d(TAG, "Adding geofences for favorited stops");
-//      Log.d(TAG, stopFavorites.toString());
-//      for (StopFavorite stopFavorite : stopFavorites) {
-//        // TODO: fetch stop info
-//      }
-//    }
-//  }
-//
-//  @Override
-//  public void onSuccess(StopModel stopModel) {
-//    if (stopModel != null) {
-//      busStopGeofenceService.registerGeofence(stopModel);
-//    }
-//  }
-//
-//  @Override
-//  public void onException(Exception e) {
-//    Log.e(TAG, e.getLocalizedMessage());
-//  }
+  @Override
+  public void handleReceive(Context context, Intent intent) {
+    Log.d(TAG, "Received boot");
+    if (GoogleApiAvailability.getInstance()
+        .isGooglePlayServicesAvailable(context) != ConnectionResult.SUCCESS) {
+      Log.e(TAG, context.getString(R.string.no_play_services));
+    } else {
+      List<FavoriteStop> favoriteStops = favoriteStopService.getFavoriteStops();
+      Log.d(TAG, "Adding geofences for favorited stops");
+      Log.d(TAG, favoriteStops.toString());
+      for (FavoriteStop favoriteStop : favoriteStops) {
+        stopService.get(favoriteStop.getCode(), this, this);
+      }
+    }
+  }
+
+  @Override
+  public void onResult(Stop result) {
+    busStopGeofenceService.registerGeofence(result);
+  }
+
+  @Override
+  public void onException(Exception e) {
+    e.printStackTrace();
+  }
 }
